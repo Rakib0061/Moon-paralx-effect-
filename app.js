5e7d1849acb884feaa773b9dc6bb9229fea471e4
Rakib0061 <rakibulislam0061@gmail.com>
2023-03-06 18:48:26 +0600

    add paralax effect

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // @@@@@@@@@@@@@@@@@@@@@@@@@  preloader  @@@@@@@@@@@@@@@@@@@@@@@@@
 
-// ----------------------------P1----------------------------
+// ----------------------------preloader animation----------------------------
 
 /* get "p" tag in this file */
 let motion = document.getElementById("motion");
@@ -10,25 +10,93 @@ motion.innerHTML = motion.innerHTML.split("").map((val,ind)=>
     `<span style="transform: rotate(${ind*9.8}deg)"> ${val} </span>`
 ).join("");
 
-// ----------------------------P1----------------------------
+// ----------------------------preloader animation----------------------------
 
 
 
 
 
 
-// ----------------------------P2----------------------------
+// ----------------------------load event----------------------------
 
 let preloader = document.getElementById("preloader");
 window.addEventListener("load",()=>{
     setTimeout(() => {
         preloader.style.display = "none";
-    }, 2000);
+        document.body.style.overflowY = "scroll";
+    }, 1000);
 });
 
-// ----------------------------P2----------------------------
+// ----------------------------load event----------------------------
 
 
+
+
+
+
+// ----------------------------on refresh scroll top----------------------------
+
+if(history.scrollRestoration){
+    history.scrollRestoration = "manual";
+}
+else{    
+    window.scrollTo(0,0);
+}
+
+// ----------------------------on refresh scroll top----------------------------
 
 // @@@@@@@@@@@@@@@@@@@@@@@@@  preloader  @@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+
+
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@  Sticky navbar  @@@@@@@@@@@@@@@@@@@@@@@@@
+
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll",()=>{
+    let Y = window.scrollY;
+    navbar.classList.toggle("sticky",Y > 0);
+});
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@  Sticky navbar  @@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+
+
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@  dark mode  @@@@@@@@@@@@@@@@@@@@@@@@@
+
+let dark_input = document.getElementById("dark_mode");
+
+dark_input.addEventListener("click",()=>{
+    document.body.classList.toggle("dark_theme");
+});
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@  dark mode  @@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+
+
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@  parallax effect  @@@@@@@@@@@@@@@@@@@@@@@@@
+
+let star = document.getElementById("star");
+let moon = document.getElementById("moon");
+let mountain = document.getElementById("mountain");
+
+window.addEventListener("scroll",()=>{
+    let Y = window.scrollY;
+
+    let star_p = Y * 0.2;
+    let moon_p = Y * 0.4;
+    let mountain_p = Y * 0.09;
+
+    star.style.transform = `translateX(${star_p}px)`;
+    moon.style.transform = `translateY(${moon_p}px)`;
+    mountain.style.transform = `translateY(${mountain_p}px)`;
+});
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@  parallax effect  @@@@@@@@@@@@@@@@@@@@@@@@@
 
