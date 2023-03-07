8c61ed60f1eead4cd4c5842ce852cb9d3463f254
Rakib0061 <rakibulislam0061@gmail.com>
2023-03-08 03:07:37 +0600

    add clock on this page

--- app.js
+++ app.js
@@ -100,3 +100,12 @@ window.addEventListener("scroll",()=>{
 
 // @@@@@@@@@@@@@@@@@@@@@@@@@  parallax effect  @@@@@@@@@@@@@@@@@@@@@@@@@
 
+
+
+
+let clock = document.getElementById("clock");
+
+setInterval(() => {
+    let time = new Date().toLocaleTimeString();
+    clock.innerHTML = time;
+}, 1000);
